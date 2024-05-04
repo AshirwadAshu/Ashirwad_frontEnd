ac3a8887c3d549e7c06b7812242f7df7d1864aca
AshirwadAshu <a1ashirwad@gmail.com>
2024-05-04 20:08:06 +0530

    updated file stracture

---
+++ reactApp/screens/portfoliopage/router-demo/src/App.js
@@ -0,0 +1,28 @@
+
+ import './App.css';
+import FormScreen from './component/FormScreen';
+
+
+//  import CardImage from './component/CardImage';
+
+ //import PortfolioPage from './component/PortfolioPage';
+
+
+//  import Table from './component/Table';
+//  import TypesExample from "./component/TypesExample";
+
+function App() {
+  return (
+    <div className="App">
+        
+          <FormScreen/> 
+      
+      
+      
+      
+      
+    </div>
+  );
+}
+
+export default App;
